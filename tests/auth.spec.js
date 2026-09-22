a92056fb53635d3006fb15bc6f1dfcb423281d1a
giovanemedeiros <giovanem-contato@hotmail.com>
2026-09-22 15:34:26 -0300

    feat: add invalid login test scenario (CT03) and refactor CT02 with API setup

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import fs from 'fs';
 import path from 'path';
 
-// Helper Functions
+// Helper Function
 function getNextUserNumber() {
   const filePath = path.resolve('counter.json');
   let currentNumber = 1;
@@ -17,15 +17,10 @@ function getNextUserNumber() {
   return currentNumber;
 }
 
-//
-// Tests suite
-//
-
-// [CT01] Register a new user
+// ---- [CT01] Register a new user ----
 test.describe('Authentication - Serverest', () => {
 
   test('Should register a new user successfully', async ({ page }) => {
-    // Navigate to registration page
     await page.goto('https://front.serverest.dev/cadastrarusuarios');
 
     // Get incremental number and fill registration form
@@ -51,35 +46,57 @@ test.describe('Authentication - Serverest', () => {
     await page.waitForTimeout(5000);
   });
 
-  // [CT02] Login with valid credentials
-    test('Should login with valid credentials', async ({ page }) => {
-
-    // Get incremental number for login form
-    const userNumber = getNextUserNumber() -1;
+  // ---- [CT02] Login with valid credentials ----
+  test('Should login with valid credentials', async ({ page, request }) => {
+    // Get incremental number and create user via API
+    const userNumber = getNextUserNumber();
+    const randomUser = `testqav${userNumber}`;
     const randomEmail = `testqav${userNumber}@email.com`;
     
-    // Navigate to login page
+    // Silent user registration via API (Background)
+    await request.post('https://serverest.dev/usuarios', {
+      data: {
+        nome: randomUser,
+        email: randomEmail,
+        password: 'testqa26',
+        administrador: 'true'
+      }
+    });
+
+    // UI visual test on login page
     await page.goto('https://front.serverest.dev/login');
     await page.waitForTimeout(2000);
-    await page.getByTestId('email').click();
-    await page.waitForTimeout(2000);
     await page.getByTestId('email').fill(randomEmail);
-    await page.waitForTimeout(2000);
-    await page.getByTestId('senha').click();
     await page.waitForTimeout(2000);
     await page.getByTestId('senha').fill('testqa26');
     await page.waitForTimeout(2000);
     await page.getByTestId('entrar').click();
     await page.waitForTimeout(5000);
-
+    
     // Validate successful login
     await expect(page).toHaveURL('https://front.serverest.dev/admin/home');
     await expect(page.getByText(/bem vindo/i)).toBeVisible();
+  });
+
+  // ---- [CT03] Login with not valid credentials ----
+  test('Should show error message when logging in with invalid credentials', async ({ page }) => {
+
+    await page.goto('https://front.serverest.dev/login');
+    await page.waitForTimeout(2000);
+    await page.getByTestId('email').click();
+    await page.waitForTimeout(2000);
+    await page.getByTestId('email').fill('invalido@email.com');
+    await page.waitForTimeout(2000);
+    await page.getByTestId('senha').click();
+    await page.waitForTimeout(2000);
+    await page.getByTestId('senha').fill('123456');
+    await page.waitForTimeout(2000);
+    await page.getByTestId('entrar').click();
+    await page.waitForTimeout(2000);
+    await expect(page.getByText('Email e/ou senha inválidos')).toBeVisible();
     await page.waitForTimeout(5000);
   });
 
-  // [CT03]
-
-  // [CT04]
+  // ---- [CT04] ----
 
 });
